5876abdab9b946fc98ff47f4dbcc79389ced7287
Katharine C Humble <katharinechumble@gmail.com>
2021-11-07 18:52:51 -0500

    Added client folder and structure

---
+++ client/src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+import Navbar from "./components/Navbar";
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const secret = "mysecretsshhhhh";
+const secret = "area51";
 const expiration = "2h";
 
 module.exports = {

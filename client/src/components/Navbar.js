3970c55e03f210f442fa273af50c586c3f55daca
Daniel Zamanillo <dzamanillok@gmail.com>
2021-11-10 10:38:50 -0600

    cleaned nav

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,36 +7,42 @@ import LoginIcon from "@mui/icons-material/Login";
 import AssignmentIcon from "@mui/icons-material/Assignment";
 import SearchIcon from "@mui/icons-material/Search";
 import SavedSearchIcon from "@mui/icons-material/SavedSearch";
+import Paper from "@mui/material/Paper";
+import { useHistory } from "react-router";
 
 // import Auth from "../utils/auth";
 
 const NavBar = () => {
+	const history = useHistory();
+
 	return (
 		<>
-			<BottomNavigation showLabels>
-				<BottomNavigationAction
-					onClick={() => window.location.assign("/")}
-					label="Search"
-					icon={<SearchIcon />}
-				/>
+			<Paper elevation={3}>
+				<BottomNavigation showLabels>
+					<BottomNavigationAction
+						onClick={() => history.push("/")}
+						label="Search"
+						icon={<SearchIcon />}
+					/>
 
-				<BottomNavigationAction
-					onClick={() => window.location.assign("/saveditems")}
-					label="Saved Items"
-					icon={<SavedSearchIcon />}
-				/>
-				<BottomNavigationAction
-					onClick={() => window.location.assign("/login")}
-					label="Login"
-					icon={<LoginIcon />}
-				/>
+					<BottomNavigationAction
+						onClick={() => history.push("/saveditems")}
+						label="Saved Items"
+						icon={<SavedSearchIcon />}
+					/>
+					<BottomNavigationAction
+						onClick={() => history.push("/login")}
+						label="Login"
+						icon={<LoginIcon />}
+					/>
 
-				<BottomNavigationAction
-					onClick={() => window.location.assign("/signup")}
-					label="Sign Up"
-					icon={<AssignmentIcon />}
-				/>
-			</BottomNavigation>
+					<BottomNavigationAction
+						onClick={() => history.push("/signup")}
+						label="Sign Up"
+						icon={<AssignmentIcon />}
+					/>
+				</BottomNavigation>
+			</Paper>
 		</>
 	);
 };

732124d43480d3bb533e0df415c3091404fef870
Daniel Zamanillo <dzamanillok@gmail.com>
2021-11-11 10:46:29 -0600

    users saved items now apper in cart after page refreash. button still needs to tell user if item is already in cart. still need remove product button.

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,16 +1,23 @@
-import gql from "graphql-tag";
+import { gql } from "@apollo/client";
 
 // Query to retrieve an existing user and their lists, expect to update once the list functions have been determined
 
 export const GET_ME = gql`
-    {
-        me {
-            _id
-            firstname
-            lastname
-            username
-            email
-            savedLists{}
-        }
-    }
+	{
+		me {
+			_id
+			firstname
+			lastname
+			username
+			email
+			savedProducts {
+				itemId
+				itemName
+				price
+				imgUrl
+				buyUrl
+				description
+			}
+		}
+	}
 `;

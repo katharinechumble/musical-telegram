5c70cc24122a91840e0ad73decb9f85177792775
Katharine C Humble <katharinechumble@gmail.com>
2021-11-16 21:15:03 -0500

    updated layout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -16,60 +16,80 @@ import { useHistory } from "react-router";
 // import Auth from "../utils/auth";
 
 const NavBar = () => {
-	const history = useHistory();
+  const history = useHistory();
 
-	return (
-		<>
-			<Paper elevation={3}>
-				{/* Search */}
-				<BottomNavigation showLabels>
-					<BottomNavigationAction
-						onClick={() => history.push("/")}
-						label="Search"
-						icon={<SearchIcon />}
-					/>
-					{/* Lists */}
-					{Auth.loggedIn() ? (
-						<BottomNavigationAction
-							onClick={() => history.push("/lists")}
-							label="Your Lists"
-							icon={<ReorderIcon />}
-						/>
-					) : null}
-					{/* Cart */}
-					<BottomNavigationAction
-						onClick={() => history.push("/cart")}
-						label="Cart"
-						icon={<ShoppingCartIcon />}
-					/>
-					{/* Sign Up */}
-					{Auth.loggedIn() ? null : (
-						<BottomNavigationAction
-							onClick={() => history.push("/signup")}
-							label="Sign Up"
-							icon={<AssignmentIcon />}
-						/>
-					)}
-					{/* Log Out */}
+  return (
+    <>
+      <Paper elevation={3}>
+        {/* Search */}
+        <BottomNavigation showLabels>
+          <BottomNavigationAction
+            sx={{
+              backgroundColor: "#A5D8F3",
+              color: "#072636",
+            }}
+            onClick={() => history.push("/")}
+            label="Search"
+            icon={<SearchIcon />}
+          />
+          {/* Lists */}
+          {Auth.loggedIn() ? (
+            <BottomNavigationAction
+              sx={{
+                backgroundColor: "#A5D8F3",
+                color: "#072636",
+              }}
+              onClick={() => history.push("/lists")}
+              label="Your Lists"
+              icon={<ReorderIcon />}
+            />
+          ) : null}
+          {/* Cart */}
+          <BottomNavigationAction
+            sx={{
+              backgroundColor: "#A5D8F3",
+              color: "#072636",
+            }}
+            onClick={() => history.push("/cart")}
+            label="Cart"
+            icon={<ShoppingCartIcon />}
+          />
+          {/* Sign Up */}
+          {Auth.loggedIn() ? null : (
+            <BottomNavigationActio
+              sx={{
+                backgroundColor: "#A5D8F3",
+                color: "#072636",
+              }}
+              onClick={() => history.push("/signup")}
+              label="Sign Up"
+              icon={<AssignmentIcon />}
+            />
+          )}
+          {/* Log Out */}
 
-					{Auth.loggedIn() ? (
-						<BottomNavigationAction
-							onClick={() => Auth.logout()}
-							label="Logout"
-							icon={<LogoutIcon />}
-						/>
-					) : (
-						// Log In
-						<BottomNavigationAction
-							onClick={() => history.push("/login")}
-							label="Login"
-							icon={<LoginIcon />}
-						/>
-					)}
-				</BottomNavigation>
-			</Paper>
-		</>
-	);
+          {Auth.loggedIn() ? (
+            <BottomNavigationAction
+              sx={{
+                backgroundColor: "#A5D8F3",
+                color: "#072636",
+              }}
+              onClick={() => Auth.logout()}
+              label="Logout"
+              icon={<LogoutIcon />}
+            />
+          ) : (
+            // Log In
+            <BottomNavigationAction
+              onClick={() => history.push("/login")}
+              label="Login"
+              icon={<LoginIcon />}
+            />
+          )}
+        </BottomNavigation>
+      </Paper>
+    </>
+  );
 };
 
 export default NavBar;

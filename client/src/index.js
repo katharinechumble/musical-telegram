5b06e658039bd8a697515062366d9fc2ebb9b0ad
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-11-18 07:16:30 -0600

    commented out the sw per breaking points

--- client/src/index.js
+++ client/src/index.js
@@ -6,21 +6,21 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import App from "./App";
+// import * as serviceWorker from "./serviceworker";
 
-// if ("serviceWorker" in navigator) {
-// 	window.addEventListener("load", () => {
-// 	  navigator.serviceWorker.register('/serviceworker.js')
-// 	  .then((reg) => {
-// 		console.log("service worker registered!", reg);
-// 	  });
-// 	});
-//   }
+// // if ("serviceWorker" in navigator) {
+// //   window.addEventListener("load", () => {
+// //     navigator.serviceWorker.register("/serviceworker.js").then((reg) => {
+// //       console.log("service worker registered!", reg);
+// //     });
+// //   });
+// // }
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
-serviceWorker.register();
+// serviceWorker.register();
